3fa617e71312d2188de400efee2b3187501e9ace
Abhishek Negi <negi.abhishek989@gmail.com>
2021-02-15 13:58:35 +0530

    JSON Intersection added

--- src/main/java/JsonObject.java
+++ src/main/java/JsonObject.java
@@ -1,16 +1,30 @@
 import org.json.JSONObject;
 public class JsonObject extends JSONObject{
 
+    /**
+     *
+     * @param path
+     * @param key
+     * @param Value
+     */
     public void addToPath(String path,String key, Object Value){
        String[]pathList = path.split("\\.");
        addValue(pathList,1,key,Value,this);
     }
 
+    /**
+     * Recursively add search for the path and put the specified value on the key
+     * @param pathList
+     * @param level
+     * @param key
+     * @param value
+     * @param json
+     * @return
+     */
     private Object addValue(String[] pathList,int  level, String key, Object value,JSONObject json){
         if(pathList.length<=level)
             return json;
         if(level == pathList.length-1 && json.has(pathList[level]) ){
-//            json.put(key,value);
             Object valueAtPath = json.get(pathList[level]);
             if(valueAtPath instanceof JSONObject)
                 json.getJSONObject(pathList[level]).put(key,value);
@@ -31,4 +45,29 @@ public class JsonObject extends JSONObject{
         }
         return  null;
     }
+
+    /**
+     *
+     * @param json1
+     * @param json2
+     * @return
+     */
+    public JSONObject intersection(JSONObject json1, JSONObject json2){
+        return getIntersection(json1,json2);
+    }
+
+    private JSONObject getIntersection(JSONObject json1, JSONObject json2){
+        JSONObject intersectedValues = new JSONObject();
+        json1.keySet().forEach(key->{
+            if(json2.has(key) && json1.get(key) instanceof JSONObject && json2.get(key) instanceof JSONObject){
+                intersectedValues.put(key,getIntersection(json1.getJSONObject(key),json2.getJSONObject(key)));
+            }
+            else if(json2.has(key) && json1.get(key).getClass()== json2.get(key).getClass()
+            && json1.get(key)==json2.get(key)){
+                intersectedValues.put(key,json1.get(key));
+            }
+        });
+        return intersectedValues;
+    }
+
 }
--- src/test/java/JsonObjectTest.java
+++ src/test/java/JsonObjectTest.java
@@ -1,5 +1,6 @@
 import org.json.JSONObject;
 import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
 
 public class JsonObjectTest {
     @Test
@@ -9,5 +10,22 @@ public class JsonObjectTest {
         System.out.println(json);
         json.addToPath("$.key1","key3",new JSONObject("{\"boolean\":true}"));
         System.out.println(json.toString(3));
+    }
+
+    @Test
+    @DisplayName("Intersection Test")
+    public void testIntersection(){
+        JSONObject json1 = new JSONObject("{\"key1\":{\"inside\":\"Value\",\"insideKey2\":true},\"key2\":false}");
+        JSONObject json2 = new JSONObject("{\"key1\":{\"insideKey2\":true}}");
+        JsonObject json=new JsonObject();
+        System.out.println(json.intersection(json1,json2));
+        // Checking for inner key comparison
+         json1 = new JSONObject("{\"key1\":{\"inside\":\"Value\",\"insideKey2\":true},\"key2\":false}");
+         json2 = new JSONObject("{\"key1\":{\"insideKey2\":false}}");
+        System.out.println(json.intersection(json1,json2));
+        //checking for json array
+        json1 = new JSONObject("{\"key1\":[\"a\",\"b\"]}");
+        json2 = new JSONObject("{\"key1\":[\"a\",\"b\"]}");
+        System.out.println(json.intersection(json1,json2));
     }
 }
